fd14e15b646153ec8d79e5488d5260c143eccaba
Xh3ni <xhensilastambolliu@yahoo.com>
2023-03-21 22:43:38 +0000

    Add, edit, delete a job post as an admin

--- recruiters/views.py
+++ recruiters/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+from .models import Job, Applicants, Selected
+from candidates.models import Profile, Skill
+from .forms import NewJobForm
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 
-# Create your views here.
+def rec_details():
+    context = {
+        'rec_home_page': "active",
+        'rec_navbar':1,
+    }
+
+
+def add_job(request):
+    user = request.user
+    if request.method == "POST":
+        form = NewJobForm(request.POST)
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.recruiter = user
+            data.save()
+            return redirect('job-list')
+    else:
+        form = NewJobForm()
+    context = {
+        'add_job_page': "active",
+        'form': form,
+        'rec_navbar': 1,
+    }
+    return render(request, 'recruiters/add_job.html', context)
+
+
+
