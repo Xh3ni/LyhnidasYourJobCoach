6264907e6f3475779f27e67c4ccba52c18456c55
Xh3ni <xhensilastambolliu@yahoo.com>
2023-03-21 19:41:03 +0000

    Create Profile and Skill models for Candidate

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django_countries.fields import CountryField
+from django.utils import timezone
+from autoslug import AutoSlugField
 
-# Create your models here.
+CHOICES = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+    ('Internship', 'Internship'),
+    ('Remote', 'Remote'),
+)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
+    full_name = models.CharField(max_length=200, null=True, blank=True)
+    profile_image = CloudinaryField('image', default='placeholder')
+    country = CountryField(null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    resume = models.FileField(upload_to='resumes', null=True, blank=True)
+    grad_year = models.IntegerField(blank=True)
+    looking_for = models.CharField(
+        max_length=30, choices=CHOICES, default='Full Time', null=True)
+    slug = AutoSlugField(populate_from='user', unique=True)
+
+    def get_absolute_url(self):
+        return "/profile/{}".format(self.slug)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Skill(models.Model):
+    skill = models.CharField(max_length=200)
+    user = models.ForeignKey(
+        User, related_name='skills', on_delete=models.CASCADE)
