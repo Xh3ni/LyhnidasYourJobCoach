f9357f2603cf4b38ad839756a6df22b001236fa2
Xh3ni <xhensilastambolliu@yahoo.com>
2023-03-21 20:13:06 +0000

    Create admin site for Candidates

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Profile, Skill
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(SummernoteModelAdmin):
+
+    list_display = ('full_name', 'slug', 'country', 'looking_for')
+    search_fields = ['country', 'looking_for']
+    list_filter = ('user', 'looking_for')
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('user', 'skill')
+    search_fields = ['user', 'skill']
+    list_filter = ('user', 'skill')
